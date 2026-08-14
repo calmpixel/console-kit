3f3cab04d76b235ebc1f77d3cd4eb11a16d9d227
calmpixel <calmpixel@local>
2026-08-14 23:06:37 +0800

    style(branding): 菱形 logo 改对称四切面（宝石切面，去歪斜感）

--- src/components/common/system-logo.vue
+++ src/components/common/system-logo.vue
@@ -8,24 +8,28 @@
     aria-hidden="true"
   >
     <defs>
-      <linearGradient id="cs-g" x1="18" y1="18" x2="110" y2="110" gradientUnits="userSpaceOnUse">
-        <stop offset="0" stop-color="rgb(var(--primary-300-color))" />
-        <stop offset="0.5" stop-color="rgb(var(--primary-500-color))" />
-        <stop offset="1" stop-color="rgb(var(--primary-700-color))" />
-      </linearGradient>
-      <linearGradient id="cs-m" x1="18" y1="18" x2="110" y2="110" gradientUnits="userSpaceOnUse">
-        <stop offset="0" stop-color="rgb(var(--primary-200-color))" />
-        <stop offset="1" stop-color="rgb(var(--primary-400-color))" />
-      </linearGradient>
-      <linearGradient id="cs-t" x1="18" y1="18" x2="110" y2="110" gradientUnits="userSpaceOnUse">
+      <linearGradient id="cs-up" x1="64" y1="6" x2="64" y2="64" gradientUnits="userSpaceOnUse">
         <stop offset="0" stop-color="rgb(var(--primary-100-color))" />
         <stop offset="1" stop-color="rgb(var(--primary-300-color))" />
       </linearGradient>
+      <linearGradient id="cs-dn" x1="64" y1="122" x2="64" y2="64" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="rgb(var(--primary-600-color))" />
+        <stop offset="1" stop-color="rgb(var(--primary-400-color))" />
+      </linearGradient>
+      <linearGradient id="cs-lf" x1="6" y1="64" x2="64" y2="64" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="rgb(var(--primary-300-color))" />
+        <stop offset="1" stop-color="rgb(var(--primary-500-color))" />
+      </linearGradient>
+      <linearGradient id="cs-rt" x1="122" y1="64" x2="64" y2="64" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="rgb(var(--primary-300-color))" />
+        <stop offset="1" stop-color="rgb(var(--primary-500-color))" />
+      </linearGradient>
     </defs>
-    <!-- 三层菱形错位叠加（颜色叠加，不做镂空） -->
-    <path d="M64 6 L122 64 L64 122 L6 64 Z" fill="url(#cs-g)" />
-    <path d="M74 14 L118 58 L74 102 L30 58 Z" fill="url(#cs-m)" opacity="0.9" />
-    <path d="M80 24 L110 54 L80 84 L50 54 Z" fill="url(#cs-t)" />
+    <!-- 菱形四切面（对称宝石切面，多面渐变） -->
+    <path d="M64 6 L122 64 L6 64 Z" fill="url(#cs-up)" />
+    <path d="M122 64 L64 6 L64 122 Z" fill="url(#cs-rt)" />
+    <path d="M64 122 L6 64 L122 64 Z" fill="url(#cs-dn)" />
+    <path d="M6 64 L64 122 L64 6 Z" fill="url(#cs-lf)" />
   </svg>
 </template>
 
